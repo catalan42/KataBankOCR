3a5c13d058ae083afbea980724a6b04217dee0b0
Alan Thompson <thompson2526@gmail.com>
2014-02-07 15:26:36 -0800

    misc

--- src/ocr/core.clj
+++ src/ocr/core.clj
@@ -1,6 +1,21 @@
-(ns ocr.core)
+(ns ocr.core
+  (:require
+    [clojure.string  :as str]
+    [clojure.set     :as set]
+    [ocr.log         :as log]
+  ))
 
-(defn foo
+(log/set-min-level log/NORMAL)
+
+(defn do-tests 
+  "Documents (& tests) regex stuff."
+  []
+  (assert true)
+)
+(defonce test-results (do-tests) )  ; Ensure tests run once when code loaded
+
+
+(defn main
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  [& args]
+  (log/msg "Hello, World!"))
---
+++ src/ocr/log.clj
@@ -0,0 +1,37 @@
+(ns ocr.log
+  (:require
+    [clojure.string  :as str]
+  ))
+
+; Simple logging tools for demo. Replace with log4j or similar in production.
+
+(def ^:const NEVER  99 )
+(def ^:const FATAL   6 )
+(def ^:const ERROR   5 )
+(def ^:const WARN    4 )
+(def ^:const NORMAL  3 )
+(def ^:const EXTRA   2 )
+(def ^:const DEBUG   1 )
+(def ^:const TRACE   0 )
+
+(def logging-min-level  NORMAL )
+
+(defn write-to-log
+  "Write log msg to console for debugging."
+  [level & msgs]
+  (when (<= logging-min-level level)
+    (apply println msgs ) ))
+
+; Convenience functions
+(defn error  [& msgs] (apply write-to-log  ERROR    msgs ))
+(defn warn   [& msgs] (apply write-to-log  WARN     msgs ))
+(defn msg    [& msgs] (apply write-to-log  NORMAL   msgs ))
+(defn extra  [& msgs] (apply write-to-log  EXTRA    msgs ))
+(defn dbg    [& msgs] (apply write-to-log  DEBUG    msgs ))
+
+
+(defn set-min-level
+  "Sets the minimum level for log messages to be reported."
+  [level]
+  (def logging-min-level level) )
+
