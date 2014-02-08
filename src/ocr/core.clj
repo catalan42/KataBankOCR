d8f4868fcb8d0d4e6b5c19fb11d1699d90330557
Alan Thompson <thompson2526@gmail.com>
2014-02-07 17:13:37 -0800

    misc

--- src/ocr/core.clj
+++ src/ocr/core.clj
@@ -5,17 +5,34 @@
     [ocr.log         :as log]
   ))
 
-(log/set-min-level log/NORMAL)
+(log/set-min-level log/DEBUG)
+
+(def digit-patterns " #  _     _  _     _  _  _  _  _  #
+                   # | |  | _| _||_||_ |_   ||_||_| #
+                   # |_|  ||_  _|  | _||_|  ||_| _| # " )
+
+(defn demux-pat
+  "Demultiplex the pattern for each digit 0..9 from the input pattern sequence."
 
 (defn do-tests 
   "Documents (& tests) regex stuff."
   []
-  (assert true)
+  ; Pull out the patterns, composed of space, pipe, and underscore
+  (def pat-str "# [ |_]{30} #" )
+  (def digits-pat-seq 
+    (re-seq (re-pattern pat-str) digit-patterns) )
+  (assert (= digits-pat-seq [ "#  _     _  _     _  _  _  _  _  #"
+                              "# | |  | _| _||_||_ |_   ||_||_| #"
+                              "# |_|  ||_  _|  | _||_|  ||_| _| #" ] ))
+  (log/dbg)
+  (log/dbg "digit-patterns:" )
+  (doseq [ line digits-pat-seq ] (log/dbg line) )
+
 )
 (defonce test-results (do-tests) )  ; Ensure tests run once when code loaded
 
 
-(defn main
-  "I don't do a whole lot."
-  [& args]
-  (log/msg "Hello, World!"))
+(defn main [& args]
+
+)
+
--- src/ocr/log.clj
+++ src/ocr/log.clj
@@ -5,16 +5,16 @@
 
 ; Simple logging tools for demo. Replace with log4j or similar in production.
 
-(def ^:const NEVER  99 )
-(def ^:const FATAL   6 )
-(def ^:const ERROR   5 )
-(def ^:const WARN    4 )
-(def ^:const NORMAL  3 )
-(def ^:const EXTRA   2 )
-(def ^:const DEBUG   1 )
-(def ^:const TRACE   0 )
+(def ^:const NEVER   99 )
+(def ^:const FATAL    6 )
+(def ^:const ERROR    5 )
+(def ^:const WARN     4 )
+(def ^:const MESSAGE  3 )  ; normal
+(def ^:const EXTRA    2 )
+(def ^:const DEBUG    1 )
+(def ^:const TRACE    0 )
 
-(def logging-min-level  NORMAL )
+(def logging-min-level  MESSAGE )
 
 (defn write-to-log
   "Write log msg to console for debugging."
@@ -23,11 +23,13 @@
     (apply println msgs ) ))
 
 ; Convenience functions
+(defn fatal  [& msgs] (apply write-to-log  FATAL    msgs ))
 (defn error  [& msgs] (apply write-to-log  ERROR    msgs ))
 (defn warn   [& msgs] (apply write-to-log  WARN     msgs ))
-(defn msg    [& msgs] (apply write-to-log  NORMAL   msgs ))
+(defn msg    [& msgs] (apply write-to-log  MESSAGE  msgs ))
 (defn extra  [& msgs] (apply write-to-log  EXTRA    msgs ))
 (defn dbg    [& msgs] (apply write-to-log  DEBUG    msgs ))
+(defn trace  [& msgs] (apply write-to-log  TRACE    msgs ))
 
 
 (defn set-min-level
