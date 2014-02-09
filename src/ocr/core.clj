6311436c24158be4a8419daf46d3e75b21c808d1
Alan Thompson <thompson2526@gmail.com>
2014-02-08 16:46:09 -0800

    misc

--- src/ocr/core.clj
+++ src/ocr/core.clj
@@ -8,11 +8,13 @@
 (log/set-min-level log/DEBUG)
 
 (def digit-patterns " #  _     _  _     _  _  _  _  _  #
-                   # | |  | _| _||_||_ |_   ||_||_| #
-                   # |_|  ||_  _|  | _||_|  ||_| _| # " )
+                      # | |  | _| _||_||_ |_   ||_||_| #
+                      # |_|  ||_  _|  | _||_|  ||_| _| # " )
 
 (defn demux-pat
   "Demultiplex the pattern for each digit 0..9 from the input pattern sequence."
+  [pat1 pat2 pat3]
+)
 
 (defn do-tests 
   "Documents (& tests) regex stuff."
@@ -20,13 +22,27 @@
   ; Pull out the patterns, composed of space, pipe, and underscore
   (def pat-str "# [ |_]{30} #" )
   (def digits-pat-seq 
-    (re-seq (re-pattern pat-str) digit-patterns) )
+    (vec (re-seq (re-pattern pat-str) digit-patterns)) )
   (assert (= digits-pat-seq [ "#  _     _  _     _  _  _  _  _  #"
                               "# | |  | _| _||_||_ |_   ||_||_| #"
                               "# |_|  ||_  _|  | _||_|  ||_| _| #" ] ))
   (log/dbg)
   (log/dbg "digit-patterns:" )
-  (doseq [ line digits-pat-seq ] (log/dbg line) )
+  ; digits-pat-seq 
+  (def dps1 (map #(->> %
+                       (drop 2   )
+                       (take 30  )
+                       (str/join ) ) digits-pat-seq ))
+  (doseq [ line dps1 ] (log/msg line) )
+
+  (def dps2 (map (partial take 30) dps1) )
+  (doseq [ line dps2 ] (log/msg line) )
+
+  (def dps3 (map #(partition 3 %) dps2 ))
+  (doseq [line dps3] 
+    (doseq [digit line]
+      (log/msg (str/join (flatten [\" digit "\" "] )))
+    ))
 
 )
 (defonce test-results (do-tests) )  ; Ensure tests run once when code loaded
